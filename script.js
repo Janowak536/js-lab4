84604f68c2ea67463248b6ef05a2b9df9563b167
Janowak536 <janowak536@gmail.com>
2022-10-13 14:09:32 +0200

    calc update

--- script.js
+++ script.js
@@ -1,34 +1,110 @@
-const liczba1 = document.querySelector('#liczba1');
-const liczba2 = document.querySelector('#liczba2');
-const liczba3 = document.querySelector('#liczba3');
-const liczba4 = document.querySelector('#liczba4');
-const btnPrzelicz = document.querySelector('#przelicz');
-const wyniki1 = document.querySelector('#suma');
-const wyniki2 = document.querySelector('#min');
-const wyniki3 = document.querySelector('#max');
-const wyniki4 = document.querySelector('#sr');
-const wyniki5 = document.querySelector('#suma1');
-const wyniki6 = document.querySelector('#min1');
-const wyniki7 = document.querySelector('#max1');
-const wyniki8 = document.querySelector('#sr1');
-const input = document.querySelector('input');
+const addNewInput = document.getElementById("addNewInput");
+const calculate = document.getElementById("calculate");
+var liveValues = document.getElementsByTagName("input");
+const box = document.getElementById("box");
+var inputNum = 4;
+let sum = 0;
+let average = 0;
+let max = 0;
+let min = 0;
+const p = document.createElement("p");
+var numberArray = [];
+var liveNumberArray = [];
 
-btnPrzelicz.addEventListener('click', () => {
+addNewInput.addEventListener("click", () => {
+    inputNum = inputNum + 1;
 
-    wyniki1.innerHTML = parseInt(liczba1.value) + parseInt(liczba2.value) + parseInt(liczba3.value) + parseInt(liczba4.value)
-})
+    const newInput = document.createElement("input");
+    box.appendChild(newInput);
+    newInput.setAttribute("id", "input" + inputNum);
+    newInput.setAttribute("class", "deleteInput" + inputNum);
 
-btnPrzelicz.addEventListener('click', () => {
-    wyniki2.innerHTML = Math.min(parseInt(liczba1.value), parseInt(liczba2.value), parseInt(liczba3.value), parseInt(liczba4.value))
-})
-btnPrzelicz.addEventListener('click', () => {
-    wyniki3.innerHTML = Math.max(parseInt(liczba1.value), parseInt(liczba2.value), parseInt(liczba3.value), parseInt(liczba4.value))
-})
-btnPrzelicz.addEventListener('click', () => {
-    wyniki4.innerHTML = (parseInt(liczba1.value) + parseInt(liczba2.value) + parseInt(liczba3.value) + parseInt(liczba4.value)) / 4;
-})
+    const newDeleteButton = document.createElement("button");
+    box.appendChild(newDeleteButton);
+    newDeleteButton.setAttribute("id", "deleteInput" + inputNum);
+    newDeleteButton.textContent = "Usuń";
+
+    liveValues = liveValues + document.getElementsByTagName("input");
+});
+
+const buttonsId = document.getElementsByTagName("button");
+var buttonId = 0;
+
+const buttonPressed = (id) => {
+    buttonId = id.target.id;
+    const buttonToDelete = document.getElementById(buttonId);
+    const inpustToDelete = document.getElementsByClassName(buttonId);
+    var requierdElement = inpustToDelete[0];
+
+    if (buttonToDelete?.innerHTML == "Usuń") {
+        requierdElement?.remove();
+        buttonToDelete?.remove();
+    }
+};
+for (let btn of buttonsId) {
+    btn.addEventListener("click", buttonPressed);
+}
+
+calculate.addEventListener("click", () => {
+    p.remove();
+    const inputs = document.getElementsByTagName("input");
+    for (let num of inputs) {
+        if (num.value == "") {
+            num.value = 0;
+        }
+        numberArray.push(num.value);
+        sum = sum + parseInt(num.value);
+    }
+    average = sum / inputs.length;
+    max = numberArray.reduce((a, b) => {
+        return Math.max(a, b);
+    });
+    min = numberArray.reduce((a, b) => {
+        return Math.min(a, b);
+    });
+
+    const newDiv = document.createElement("div");
+    document.body.appendChild(newDiv);
+    newDiv.appendChild(p);
+    p.innerHTML =
+        "Suma = " + sum + ", " + "Średnia = " + average + ", " + "Max = " + max + ", " + "Min = " + min;
+
+    sum = 0;
+    average = 0;
+    numberArray = [];
+});
 
 
-input.addEventListener('change', () => {
-    wyniki5.innerHTML = ((parseInt(liczba1.value) || 0) + (parseInt(liczba2.value) || 0) + (parseInt(liczba3.value) || 0) + (parseInt(liczba4.value) || 0))
-})
+
+for (let liveValue of liveValues) {
+    liveValue.addEventListener("change", () => {
+        p.remove();
+        const inputs = document.getElementsByTagName("input");
+
+        for (let num of inputs) {
+            if (num.value == "") {
+                num.value = 0;
+            }
+            numberArray.push(num.value);
+            sum = sum + parseInt(num.value);
+        }
+
+        average = sum / inputs.length;
+
+        max = numberArray.reduce((a, b) => {
+            return Math.max(a, b);
+        });
+        min = numberArray.reduce((a, b) => {
+            return Math.min(a, b);
+        });
+
+        const newDiv = document.createElement("div");
+        document.body.appendChild(newDiv);
+        newDiv.appendChild(p);
+        p.innerHTML =
+            "Suma = " + sum + ", " + "Średnia = " + average + ", " + "Max = " + max + ", " + "Min = " + min;
+        sum = 0;
+        average = 0;
+        numberArray = [];
+    });
+}
