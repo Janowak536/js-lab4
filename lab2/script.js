c15ae6ab48c6e959e3aab89d17797981a6045006
Jan Nowak <jan.nowak@predicagroup.com>
2022-12-07 21:52:33 +0100

    slider update

--- lab2/script.js
+++ lab2/script.js
@@ -1,8 +1,31 @@
-var counter = 1;
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const first = document.getElementById("first");
+let counter = 0;
+btn1.addEventListener('click',function(){
+first.style.marginLeft = "0";
+counter =0;
+});
+btn2.addEventListener('click',function(){
+first.style.marginLeft = "-20%";
+counter=20;
+});
+btn3.addEventListener('click',function(){
+first.style.marginLeft = "-40%";
+counter=40;
+});
+btn4.addEventListener('click',function(){
+first.style.marginLeft = "-60%";
+counter=60;
+});
+
+
 setInterval(function () {
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if (counter > 4) {
-        counter = 1;
-    }
+    first.style.marginLeft = `-${counter}%`;
+    counter += 20;
+    if (counter >60) {
+        counter = 0;
+    } 
 }, 5000);
