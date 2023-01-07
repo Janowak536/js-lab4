8818c94122631450dfe734eebd333343bbd86f34
Jan Nowak <jan.nowak@predicagroup.com>
2023-01-07 16:22:59 +0100

    dodanie wszystkich projektów do gita

---
+++ async/script.js
@@ -0,0 +1,14 @@
+const asyncAdd = async (a,b) => {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return Promise.reject('Argumenty muszą mieć typ number!')
+  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() =>{
+      resolve(a+b)
+    }, 100)
+  })
+}
+
+function add(){
+    
+}
