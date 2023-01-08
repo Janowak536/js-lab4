a6f726e1e99aaed0fda54d5248e7ba36c2e886c8
Jan Nowak <jan.nowak@predicagroup.com>
2023-01-08 16:46:54 +0100

    async task

--- async/script.js
+++ async/script.js
@@ -9,6 +9,48 @@ const asyncAdd = async (a,b) => {
   })
 }
 
-function add(){
-    
+let args100 =[1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,];
+let args5 = [1,2,3,4,5];
+let numberOfAsyncAdd = 0;;
+var startTimer;
+var currentTime;
+var elapsedTime = 0;
+
+
+const asyncAddMultiple = async (args) => {
+  let sum = 0;
+  startStopwatch();
+  for (const arg of args) {
+    sum = await asyncAdd(sum, arg);
+    numberOfAsyncAdd++;
+  }
+  stopStopwatch();
+  console.log(`ilość operacji asynchronicznych:${numberOfAsyncAdd}`)
+  console.log(sum);
+  return sum;
 }
+
+function startStopwatch(){
+  startTimer = performance.now();
+  console.log('timer start');
+}
+
+function stopStopwatch(){
+  currentTime = performance.now();
+  elapsedTime = (currentTime - startTimer);
+  console.log(`timer stop: ${elapsedTime}`);
+}
+
+//asyncAddMultiple(args5);
+
+const asyncOptimizeAddMultiple = async (args) => {
+  startStopwatch();
+  const promises = args.map(arg => asyncAdd(0, arg));
+  const results = await Promise.all(promises);
+  const result = results.reduce((sum, result) => sum + result, 0);
+  stopStopwatch();
+  console.log(result);
+  return result;
+}
+
+asyncOptimizeAddMultiple(args100);
