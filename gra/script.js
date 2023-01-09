cc33731149fedae76d9b41c87dd215b77089ff7e
Jan Nowak <jan.nowak@predicagroup.com>
2023-01-09 23:15:40 +0100

    timer and score

--- gra/script.js
+++ gra/script.js
@@ -3,12 +3,14 @@ let board = document.querySelector('#board');
 let position = document.querySelector('#position');
 let hole = document.querySelector('.hole');
 let btn = document.getElementsByClassName('btn');
+let scoreElement = document.getElementById('points');
 let x;
 let y;
 let time;
 let result;
+let score=0;
+let prevColour = "F4F2F3";
 //game works only in 722x514
-
 let posX = board.clientWidth - ball.clientWidth;
 let posY = board.clientHeight - ball.clientHeight;
 
@@ -36,34 +38,65 @@ window.addEventListener('deviceorientation', function(event) {
   var beta = event.beta; 
   checkIfIsInHole(gamma,beta);
   checkIfTouchRed(gamma,beta);
+  makeScreenWhite(gamma,beta);
 });
 
+function displayScore(point) {
+    scoreElement.innerHTML = point;
+} 
 
 function checkIfIsInHole(gamma, beta){
-    if (gamma <= -48 && gamma >= -57 && beta <= 0 && beta >= -4){
+    if (gamma <= -36 && gamma >= -49 && beta <= 11 && beta >= 6){
         win();
     }
 }
 
+function increaseScore() {
+    score++;
+    displayScore(score);
+  }
+
+function makeScreenWhite(gamma,beta){
+    if(!((gamma <= -73 && gamma >= -78 && beta <= 8 && beta >= 4) ||
+    (gamma <= -65 && gamma >= -72 && beta <= -16 && beta >= -19)||
+    (gamma <= -3 && gamma >= -11 && beta <= 32 && beta >= 29) ||
+    (gamma <= -36 && gamma >= -49 && beta <= 11 && beta >= 6))){
+        board.style.background = ("#F4F2F3");
+        prevColour ="F4F2F3"
+    }
+}
 
 function checkIfTouchRed(gamma,beta)
 {
-    if(gamma <= -4 && gamma >= -11 && beta <= 22 && beta >= 18) 
+    if(gamma <= -73 && gamma >= -78 && beta <= 8 && beta >= 4) 
         lost();
-    else if(gamma <= -70 && gamma >= -75 && beta <= -28 && beta >= -30)
+    else if(gamma <= -65 && gamma >= -72 && beta <= -16 && beta >= -19)
         lost();
-    else if(gamma <= -76 && gamma >= -81 && beta <= -3 && beta >= -6)    
+    else if(gamma <= -3 && gamma >= -11 && beta <= 32 && beta >= 29)    
         lost();
-    }
+}
 
 function lost(){
     board.style.background = ("red");
+    prevColour = "red";
 }
 function win(){
     board.style.background = ("green");
+    if ( prevColour === "F4F2F3") {
+        increaseScore();
+    }
+    prevColour = "green";
 }
 function startNew(){
     board.style.background = ("#F4F2F3");
-    ball.beta=0;
+    score=0;
+    displayScore(score);
+    setTimeout(resetScore, 60000);
 }
+
+function resetScore() {
+    score = 0;
+    displayScore(score);
+}
+setTimeout(resetScore, 60000);
 window.addEventListener('deviceorientation', Orientation)
